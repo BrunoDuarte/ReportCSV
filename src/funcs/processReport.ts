8609a42d40740c15b2d4041804d0ec454fbcee1b
BrunoCDuarte <frank.bruno555@gmail.com>
2020-09-02 17:24:29 -0300

    comments removed

--- src/funcs/processReport.ts
+++ src/funcs/processReport.ts
@@ -9,23 +9,10 @@ class processReport {
 
     public create(sprintCapacity: Sprint[], epicSize: string[]) {
 
-        // const indexSprint = sprintCapacity.findIndex(function(todo, index) {
-        //     return todo
-        // })
-
         sprintCapacity.map((key, value) => {
             console.log(`${key.Sprint} -> ${key.Capacity}`)
         })
 
-        
-
-        // console.log(sprintCapacity)
-        // console.log(indexSprint)
-        // const sprintJson = sprintCapacity.map(el => el)
-
-        // console.log(sprintCapacity[0])
-
-        // console.log(Object.keys(sprintCapacity))
     }
 
 }
